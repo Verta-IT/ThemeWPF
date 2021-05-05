ac6be19fc7e1f1efbdae3d857e4f1273cadce7d4
OmegaDSM <developer.sb@outlook.com>
2021-05-05 13:05:25 +0200

    Use the startup event in App to show mian window instead of the startup uri

--- Source/OmegaDSD.ThemeWPF/App.xaml.cs
+++ Source/OmegaDSD.ThemeWPF/App.xaml.cs
@@ -13,5 +13,11 @@ namespace OmegaDSD.ThemeWPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+
+            mainWindow.Show();
+        }
     }
 }
