9a5526026014390f6c7280d73df057f011d7594a
OmegaDSM <developer.sb@outlook.com>
2021-05-05 13:20:48 +0200

    Add main window view model and an example 'PersonModel' model

---
+++ Source/OmegaDSD.ThemeWPF/Models/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OmegaDSD.ThemeWPF.Models
+{
+    public class PersonModel
+    {
+        public PersonModel(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ Source/OmegaDSD.ThemeWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using OmegaDSD.ThemeWPF.Models;
+using OmegaDSD.ThemeWPF.Themes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace OmegaDSD.ThemeWPF.ViewModels
+{
+    public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged<T>(ref T property, T newValue, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            property = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class MainWindowViewModel : NotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            PersonCollection.Add(new PersonModel("Josh", 20));
+            PersonCollection.Add(new PersonModel("Sebastian", 25));
+            PersonCollection.Add(new PersonModel("Alex", 50));
+        }
+
+        public ObservableCollection<PersonModel> PersonCollection { get; } = new ObservableCollection<PersonModel>();
+
+        public Theme? SelectedTheme
+        {
+            get => ThemeManager.CurrentTheme;
+            set
+            {
+                if (value != null)
+                {
+                    ThemeManager.ChangeTheme((Theme)value);
+                }
+                else
+                {
+                    ThemeManager.RemoveTheme();
+                }
+
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
