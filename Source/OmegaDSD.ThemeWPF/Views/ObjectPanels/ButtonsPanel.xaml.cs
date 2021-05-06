2946fc17c939fb47f3e7c7239fc1fcc849d623a0
OmegaDSM <developer.sb@outlook.com>
2021-05-06 09:53:14 +0200

    Add 'ButtonsPanel' panel

---
+++ Source/OmegaDSD.ThemeWPF/Views/ObjectPanels/ButtonsPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OmegaDSD.ThemeWPF.Views.ObjectPanels
+{
+    /// <summary>
+    /// Interaction logic for ButtonsPanel.xaml
+    /// </summary>
+    public partial class ButtonsPanel : UserControl
+    {
+        public ButtonsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
