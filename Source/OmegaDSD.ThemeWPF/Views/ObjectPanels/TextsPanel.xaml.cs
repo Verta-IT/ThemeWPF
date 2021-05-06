11cf69495509578a77c214d39ec2af0bd7cedf0c
OmegaDSM <developer.sb@outlook.com>
2021-05-06 09:47:22 +0200

    Add 'TextBoxesPanel' and 'TextsPanel' panels

---
+++ Source/OmegaDSD.ThemeWPF/Views/ObjectPanels/TextsPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace OmegaDSD.ThemeWPF.Views.ObjectPanels
+{
+    /// <summary>
+    /// Interaction logic for TextsPanel.xaml
+    /// </summary>
+    public partial class TextsPanel : UserControl
+    {
+        public TextsPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
