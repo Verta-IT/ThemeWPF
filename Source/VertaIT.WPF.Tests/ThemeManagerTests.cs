6eb7c5ece4b0ce67810e5fa7e8a2f405c70d0b97
Sebastian Babka <developer.sb@outlook.com>
2021-11-20 01:16:31 +0100

    Switch unit test target of the 'VertaIT.WPF.Tests' project from the 'VertaIT.WPF' to 'VertaIT.WPF.Theme' project

--- Source/VertaIT.WPF.Tests/ThemeManagerTests.cs
+++ Source/VertaIT.WPF.Tests/ThemeManagerTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
-using VertaIT.WPF.Themes;
+using VertaIT.WPF.Theme;
 
 namespace VertaIT.WPF.Tests
 {
